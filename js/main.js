bc1e9e77cb82b476630c54951026222f44e1a8b6
swarm <anastasiiafilipchuk@macbookilipchuk.home>
2024-03-22 22:40:27 +0100

    Fixed HW-13 responsive design

--- js/main.js
+++ js/main.js
@@ -8,3 +8,43 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 });
+
+window.addEventListener('scroll', function () {
+    var scrollPosition = window.scrollY;
+    var footerPosition = document.querySelector('.footer').getBoundingClientRect().top;
+    var buttons = document.querySelectorAll('.btn-wrap button');
+
+    if (window.innerWidth < 480 && footerPosition <= window.innerHeight) {
+        buttons.forEach(function (button) {
+            button.classList.add('hide');
+        });
+    } else {
+        buttons.forEach(function (button) {
+            button.classList.remove('hide');
+        });
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    var toggleButton = document.querySelector('.toggle-button');
+    var buttons = document.querySelectorAll('.btn-wrap button');
+    var isButtonsVisible = false;
+
+    toggleButton.addEventListener('click', function () {
+        if (!isButtonsVisible) {
+            buttons.forEach(function (button) {
+                button.classList.remove('hide');
+            });
+            isButtonsVisible = true;
+        } else {
+            buttons.forEach(function (button) {
+                button.classList.add('hide');
+            });
+            isButtonsVisible = false;
+        }
+    });
+});
+
+document.querySelector('.toggle-button').addEventListener('click', function () {
+    this.classList.toggle('active');
+});
