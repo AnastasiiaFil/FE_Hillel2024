060ffb1df493f4f1fa8f2a31f3d472aad26686ee
swarm <anastasiiafilipchuk@macbookilipchuk.home>
2024-03-22 18:12:50 +0100

    mobile-versions changes

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', function () {
     hamburgers.forEach(function (burgerMenu) {
         burgerMenu.addEventListener('click', function () {
             this.classList.toggle('active');
+            document.querySelector('html').classList.toggle('no-scroll');
         });
     });
 });
